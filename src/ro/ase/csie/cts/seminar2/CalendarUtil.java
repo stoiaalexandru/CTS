35e6c76b74849305c565dd6fb846380c1f53064c
Andu <stoia.mihai.alexandru@gmail.com>
2021-03-04 14:27:50 +0200

    Weekday3 added

--- src/ro/ase/csie/cts/seminar2/CalendarUtil.java
+++ src/ro/ase/csie/cts/seminar2/CalendarUtil.java
@@ -2,27 +2,48 @@ package ro.ase.csie.cts.seminar2;
 
 public class CalendarUtil {
 
-    public String weekDay(int day){
-        if(day ==1)
+    public String weekDay(int day) {
+        if (day == 1)
             return "Sunday";
-        else if(day==2){
+        else if (day == 2)
             return "Monday";
-        }
-        else if(day==3){
+        else if (day == 3)
             return "Tuesday";
-        }
-        else if(day==4){
+        else if (day == 4)
             return "Wednesday";
-        }
-        else if(day==5){
+        else if (day == 5)
             return "Thursday";
-        }
-        else if(day==6){
+        else if (day == 6)
             return "Friday";
-        }
-        else if(day==7){
+        else if (day == 7)
             return "Saturday";
-        }
+
         return null;
+    }
+
+    public String weekDay2(int day) {
+        switch (day) {
+            case 1:
+                return "Sunday";
+            case 2:
+                return "Monday";
+            case 3:
+                return "Tuesday";
+            case 4:
+                return "Wednesday";
+            case 5:
+                return "Thursday";
+            case 6:
+                return "Friday";
+            case 7:
+                return "Saturday";
+            default:
+                return null;
+        }
+    }
+
+    public String weekDay3(int day) {
+        String[] weekDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        return weekDays[day - 1];
     }
 }
