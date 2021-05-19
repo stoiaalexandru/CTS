9732965386be8ed82a5fe45ca7e84c4dc293fe53
stoiaalexandru <63856047+stoiaalexandru@users.noreply.github.com>
2021-05-19 15:29:36 +0300

    Testare unitara. Code coverage

--- src/ro/ase/csie/cts/seminar12/testare/modele/OperatiiMatematice.java
+++ src/ro/ase/csie/cts/seminar12/testare/modele/OperatiiMatematice.java
@@ -12,7 +12,7 @@ public class OperatiiMatematice {
         if(b == 0){
             throw new ExceptieImpartireLaZero();
         }
-        return a/b;
+        return (double)a/b;
     }
 
 }
--- src/ro/ase/csie/cts/seminar12/testare/teste/OperatiiMatematiceTest.java
+++ src/ro/ase/csie/cts/seminar12/testare/teste/OperatiiMatematiceTest.java
@@ -1,21 +1,36 @@
 package ro.ase.csie.cts.seminar12.testare.teste;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import ro.ase.csie.cts.seminar12.testare.exceptii.ExceptieImpartireLaZero;
 import ro.ase.csie.cts.seminar12.testare.modele.OperatiiMatematice;
 
 import static org.junit.Assert.*;
 
-public class OperatiiMatematiceTest{
+public class OperatiiMatematiceTest {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        System.out.println("Salut");
+    }
+
 
     @Before
     public void setUp() throws Exception {
+        System.out.println("Pregatire test");
     }
+
 
     @After
     public void tearDown() throws Exception {
+        System.out.println("Sterge resurse test");
     }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        System.out.println("La revedere");
+    }
+
+
 
     @Test
     public void testAdunaValoriPozitive() {
@@ -29,7 +44,12 @@ public class OperatiiMatematiceTest{
     }
 
     @Test
-    public void testImpartire() {
-        assertEquals(1,1);
+    public void testImpartireValoriPozitive() throws ExceptieImpartireLaZero {
+        int a = 1;
+        int b = 2;
+        double rezultatAsteptat = 0.5;
+        double rezultatCalculat = OperatiiMatematice.impartire(a,b);
+
+        assertEquals("Impartire 1/2 ",rezultatAsteptat,rezultatCalculat,0);
     }
 }
